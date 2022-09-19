d8cfaf7228f9ba3f660db6d6a9eb094be50093a3
Hizkia Felix <felix@hizkifw.me>
2022-09-19 17:35:44 +0800

    feat: move windows across workspaces

--- VDHop/Form1.cs
+++ VDHop/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,6 +17,10 @@ namespace VDHop
 {
     public partial class Form1 : Form
     {
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
         private VirtualDesktop[] desktops;
 
         public Form1()
@@ -30,21 +35,43 @@ namespace VDHop
                 desktops = VirtualDesktop.GetDesktops();
             }
 
-            HotkeyManager.Current.AddOrReplace("0", Keys.Alt | Keys.D1, HandleSwitch);
-            HotkeyManager.Current.AddOrReplace("1", Keys.Alt | Keys.D2, HandleSwitch);
-            HotkeyManager.Current.AddOrReplace("2", Keys.Alt | Keys.D3, HandleSwitch);
-            HotkeyManager.Current.AddOrReplace("3", Keys.Alt | Keys.D4, HandleSwitch);
-            HotkeyManager.Current.AddOrReplace("4", Keys.Alt | Keys.D5, HandleSwitch);
-            HotkeyManager.Current.AddOrReplace("5", Keys.Alt | Keys.D6, HandleSwitch);
-            HotkeyManager.Current.AddOrReplace("6", Keys.Alt | Keys.D7, HandleSwitch);
-            HotkeyManager.Current.AddOrReplace("7", Keys.Alt | Keys.D8, HandleSwitch);
-            HotkeyManager.Current.AddOrReplace("8", Keys.Alt | Keys.D9, HandleSwitch);
-            HotkeyManager.Current.AddOrReplace("9", Keys.Alt | Keys.D0, HandleSwitch);
+            // Virtual Desktops
+            HotkeyManager.Current.AddOrReplace("s0", Keys.Alt | Keys.D1, HandleSwitch);
+            HotkeyManager.Current.AddOrReplace("s1", Keys.Alt | Keys.D2, HandleSwitch);
+            HotkeyManager.Current.AddOrReplace("s2", Keys.Alt | Keys.D3, HandleSwitch);
+            HotkeyManager.Current.AddOrReplace("s3", Keys.Alt | Keys.D4, HandleSwitch);
+            HotkeyManager.Current.AddOrReplace("s4", Keys.Alt | Keys.D5, HandleSwitch);
+            HotkeyManager.Current.AddOrReplace("s5", Keys.Alt | Keys.D6, HandleSwitch);
+            HotkeyManager.Current.AddOrReplace("s6", Keys.Alt | Keys.D7, HandleSwitch);
+            HotkeyManager.Current.AddOrReplace("s7", Keys.Alt | Keys.D8, HandleSwitch);
+            HotkeyManager.Current.AddOrReplace("s8", Keys.Alt | Keys.D9, HandleSwitch);
+            HotkeyManager.Current.AddOrReplace("s9", Keys.Alt | Keys.D0, HandleSwitch);
+
+            // Moving windows around virtual desktops
+            HotkeyManager.Current.AddOrReplace("m0", Keys.Alt | Keys.Shift | Keys.D1, HandleMoveSwitch);
+            HotkeyManager.Current.AddOrReplace("m1", Keys.Alt | Keys.Shift | Keys.D2, HandleMoveSwitch);
+            HotkeyManager.Current.AddOrReplace("m2", Keys.Alt | Keys.Shift | Keys.D3, HandleMoveSwitch);
+            HotkeyManager.Current.AddOrReplace("m3", Keys.Alt | Keys.Shift | Keys.D4, HandleMoveSwitch);
+            HotkeyManager.Current.AddOrReplace("m4", Keys.Alt | Keys.Shift | Keys.D5, HandleMoveSwitch);
+            HotkeyManager.Current.AddOrReplace("m5", Keys.Alt | Keys.Shift | Keys.D6, HandleMoveSwitch);
+            HotkeyManager.Current.AddOrReplace("m6", Keys.Alt | Keys.Shift | Keys.D7, HandleMoveSwitch);
+            HotkeyManager.Current.AddOrReplace("m7", Keys.Alt | Keys.Shift | Keys.D8, HandleMoveSwitch);
+            HotkeyManager.Current.AddOrReplace("m8", Keys.Alt | Keys.Shift | Keys.D9, HandleMoveSwitch);
+            HotkeyManager.Current.AddOrReplace("m9", Keys.Alt | Keys.Shift | Keys.D0, HandleMoveSwitch);
         }
 
         private void HandleSwitch(object sender, HotkeyEventArgs e)
         {
-            var i = Int32.Parse(e.Name);
+            var i = int.Parse(e.Name.Substring(1));
+            desktops[i].Switch();
+        }
+
+        private void HandleMoveSwitch(object sender, HotkeyEventArgs e)
+        {
+            var i = int.Parse(e.Name.Substring(1));
+
+            var hWnd = GetForegroundWindow();
+            VirtualDesktop.MoveToDesktop(hWnd, desktops[i]);
             desktops[i].Switch();
         }
 
