1e6ebcc6e4e02d1958fae3428f0fe921de0ef9f5
Hizkia Felix <felix@hizkifw.me>
2022-09-20 10:27:51 +0800

    feat: auto focus last active window

--- VDHop/Form1.cs
+++ VDHop/Form1.cs
@@ -21,7 +21,12 @@ namespace VDHop
         [DllImport("user32.dll")]
         static extern IntPtr GetForegroundWindow();
 
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr hWnd);
+
+
         private VirtualDesktop[] desktops;
+        private IntPtr[] foregroundWindows;
 
         public Form1()
         {
@@ -34,6 +39,7 @@ namespace VDHop
                 VirtualDesktop.Create();
                 desktops = VirtualDesktop.GetDesktops();
             }
+            foregroundWindows = new IntPtr[desktops.Length];
 
             // Virtual Desktops
             HotkeyManager.Current.AddOrReplace("s0", Keys.Alt | Keys.D1, HandleSwitch);
@@ -48,31 +54,57 @@ namespace VDHop
             HotkeyManager.Current.AddOrReplace("s9", Keys.Alt | Keys.D0, HandleSwitch);
 
             // Moving windows around virtual desktops
-            HotkeyManager.Current.AddOrReplace("m0", Keys.Alt | Keys.Shift | Keys.D1, HandleMoveSwitch);
-            HotkeyManager.Current.AddOrReplace("m1", Keys.Alt | Keys.Shift | Keys.D2, HandleMoveSwitch);
-            HotkeyManager.Current.AddOrReplace("m2", Keys.Alt | Keys.Shift | Keys.D3, HandleMoveSwitch);
-            HotkeyManager.Current.AddOrReplace("m3", Keys.Alt | Keys.Shift | Keys.D4, HandleMoveSwitch);
-            HotkeyManager.Current.AddOrReplace("m4", Keys.Alt | Keys.Shift | Keys.D5, HandleMoveSwitch);
-            HotkeyManager.Current.AddOrReplace("m5", Keys.Alt | Keys.Shift | Keys.D6, HandleMoveSwitch);
-            HotkeyManager.Current.AddOrReplace("m6", Keys.Alt | Keys.Shift | Keys.D7, HandleMoveSwitch);
-            HotkeyManager.Current.AddOrReplace("m7", Keys.Alt | Keys.Shift | Keys.D8, HandleMoveSwitch);
-            HotkeyManager.Current.AddOrReplace("m8", Keys.Alt | Keys.Shift | Keys.D9, HandleMoveSwitch);
-            HotkeyManager.Current.AddOrReplace("m9", Keys.Alt | Keys.Shift | Keys.D0, HandleMoveSwitch);
+            HotkeyManager.Current.AddOrReplace("m0", Keys.Alt | Keys.Shift | Keys.D1, HandleMove);
+            HotkeyManager.Current.AddOrReplace("m1", Keys.Alt | Keys.Shift | Keys.D2, HandleMove);
+            HotkeyManager.Current.AddOrReplace("m2", Keys.Alt | Keys.Shift | Keys.D3, HandleMove);
+            HotkeyManager.Current.AddOrReplace("m3", Keys.Alt | Keys.Shift | Keys.D4, HandleMove);
+            HotkeyManager.Current.AddOrReplace("m4", Keys.Alt | Keys.Shift | Keys.D5, HandleMove);
+            HotkeyManager.Current.AddOrReplace("m5", Keys.Alt | Keys.Shift | Keys.D6, HandleMove);
+            HotkeyManager.Current.AddOrReplace("m6", Keys.Alt | Keys.Shift | Keys.D7, HandleMove);
+            HotkeyManager.Current.AddOrReplace("m7", Keys.Alt | Keys.Shift | Keys.D8, HandleMove);
+            HotkeyManager.Current.AddOrReplace("m8", Keys.Alt | Keys.Shift | Keys.D9, HandleMove);
+            HotkeyManager.Current.AddOrReplace("m9", Keys.Alt | Keys.Shift | Keys.D0, HandleMove);
+        }
+
+        private int GetActiveDesktopNumber()
+        {
+            var currentDesktop = VirtualDesktop.Current;
+            return desktops.TakeWhile(d => d.Id != currentDesktop.Id).Count();
         }
 
         private void HandleSwitch(object sender, HotkeyEventArgs e)
         {
+            // Store active window in current desktop
+            var currentIdx = GetActiveDesktopNumber();
+            if (currentIdx >= 0 && currentIdx < foregroundWindows.Length)
+                foregroundWindows[currentIdx] = GetForegroundWindow();
+
+            // Switch to new desktop
             var i = int.Parse(e.Name.Substring(1));
+            if (i < 0 || i >= foregroundWindows.Length)
+                return;
+
             desktops[i].Switch();
+
+            // Focus window in new desktop
+            if (foregroundWindows[i] != IntPtr.Zero)
+                SetForegroundWindow(foregroundWindows[i]);
         }
 
-        private void HandleMoveSwitch(object sender, HotkeyEventArgs e)
+        private void HandleMove(object sender, HotkeyEventArgs e)
         {
             var i = int.Parse(e.Name.Substring(1));
+            if (i < 0 || i >= foregroundWindows.Length)
+                return;
 
             var hWnd = GetForegroundWindow();
+            if (hWnd == IntPtr.Zero)
+                return;
+
             VirtualDesktop.MoveToDesktop(hWnd, desktops[i]);
-            desktops[i].Switch();
+
+            // Set the target desktop's foreground window
+            foregroundWindows[i] = hWnd;
         }
 
         private void Form1_Resize(object sender, EventArgs e)
